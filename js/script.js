6318c640482f4d961aabf8d500a9d055e18a81f3
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2023-12-19 23:06:12 +0200

    update slider navigation function

--- js/script.js
+++ js/script.js
@@ -11,3 +11,24 @@ for (let item = 0; item < sliderItems.length; item++) {
 }
 
 slider.appendChild(navigation);
+
+for (let i = 0; i < sliderItems.length; i++) {
+
+    let item = sliderItems[i];
+    let dot =  navigation.querySelectorAll("li")[i];
+
+    // Activate Navigation item order
+    if (item.classList.contains('active')) {
+        dot.classList.add('active');
+    }
+
+    // Activate item by dot click
+    dot.addEventListener('click', ()=>{
+        sliderItems.forEach((it)=>{
+            it.classList.remove('active');
+        })
+        item.classList.add('active');
+
+    })
+
+}
