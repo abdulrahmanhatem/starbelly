7a09fbd4d3ea876f3926f25c14a5583910f2cc25
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2023-12-19 22:45:53 +0200

    create slider navigation via javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 let slider = document.querySelector('.slider')
-let sliderItems = slider.querySelector('.content');
+let sliderItems = slider.querySelectorAll('.content li');
 
-console.log(sliderItems.children);
+// Create navigation Elements
+let navigation = document.createElement('ul');
+navigation.classList.add('navigation');
+
+for (let item = 0; item < sliderItems.length; item++) {
+    let dot = document.createElement('li');
+    navigation.appendChild(dot);
+}
+
+slider.appendChild(navigation);
