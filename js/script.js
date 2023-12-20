0a6d7b6c5ce350ce3b50abd48b433b3a5ea973e4
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2023-12-20 15:05:02 +0200

    update slider functions

--- js/script.js
+++ js/script.js
@@ -1,63 +1,88 @@
 let slider = document.querySelector('.slider')
-let sliderItems = slider.querySelectorAll('.content li');
+let content = slider.querySelector('.content')
+let items = content.querySelectorAll('li');
 
 // Create navigation Elements
 let navigation = document.createElement('ul');
 navigation.classList.add('navigation');
 
-for (let item = 0; item < sliderItems.length; item++) {
+for (let it = 0; it < items.length; it++) {
     let dot = document.createElement('li');
     navigation.appendChild(dot);
 }
-
 slider.appendChild(navigation);
 
-for (let i = 0; i < sliderItems.length; i++) {
 
-    let item = sliderItems[i];
-    let previous = sliderItems[i-1];
-    let next = sliderItems[i+1];
-    let dots = navigation.querySelectorAll("li");
-    let dot =  dots[i];
-    let show;
+function refreshSlider(prev,current,next){
+    let show = [];
+    show.push(prev);
+    show.push(current);
+    show.push(next);
+    items.forEach(element => {
+        element.remove();
+    });
 
-    
+    show.forEach(el => {
+        content.appendChild(el)
+    })
+}
 
-    
+function showItems(current){
+    let item = items[current];
+    let previous = items[current-1];
+    let next = items[current+1];
 
-    // Activate Navigation item order
-    if (item.classList.contains('active')) {
-        dot.classList.add('active');
-    }
+    switch (current) {
+        case items.length -1:
+            previous = items[current-1];
+            next = items[0];
+            break;
+
+        case 0:
+            previous = items[items.length -1];
+            next = items[current+1];
+            break;
+        }
+
+    refreshSlider(previous,item, next )
+}
+
+function activateNavDot(){
+
+}
+
+for (let i = 0; i < items.length; i++) {
+
+   
+    // let dots = navigation.querySelectorAll("li");
+    // let dot =  dots[i];
+
+    showItems(i)
+
+
+    // // Activate Navigation item order
+    // if (item.classList.contains('active')) {
+    //     dot.classList.add('active');
+    // }
 
     // Activate item by dot click
     dot.addEventListener('click', ()=>{
         
-        sliderItems.forEach((it)=>{
+        items.forEach((it)=>{
             it.classList.remove('active');
             it.classList.remove('show');
         })
         dots.forEach((dt) => {
             dt.classList.remove('active');
         })
+
+        refreshSlider()
+
         item.classList.add('active');
-        dot.classList.add('active');
-        
-        switch (i) {
-            case sliderItems.length -1:
-                previous = sliderItems[i-1];
-                next = sliderItems[0];
-                break;
-    
-            case 0:
-                previous = sliderItems[sliderItems.length -1];
-                next = sliderItems[i+1];
-                break;
-        }
-        previous.classList.add('show')
-        next.classList.add('show')
-        
-        
+        // dot.classList.add('active');
+        // previous.classList.add('show')
+        // next.classList.add('show')
+
     })
 
     
